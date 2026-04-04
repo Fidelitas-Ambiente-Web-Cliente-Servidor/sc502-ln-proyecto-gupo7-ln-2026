f9d9a6253426e3a64138bad2b9ec01827765ed33
Natalia Campos A <nataliacamposagui1302@gmail.com>
2026-04-03 23:31:49 -0600

    Listo
    Login funcionando

--- Public/js/login.js
+++ Public/js/login.js
@@ -1,21 +1,27 @@
-document.getElementById("loginForm").addEventListener("submit", function(e) {
-    e.preventDefault();
+document.getElementById("btnLogin").addEventListener("click", function() {
 
     let email    = document.getElementById("email").value;
     let password = document.getElementById("password").value;
 
-    fetch("../login.php", {
+    if (email === "" || password === "") {
+        alert("Todos los campos son obligatorios.");
+        return;
+    }
+
+    fetch("../index.php", {
         method: "POST",
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: "email=" + encodeURIComponent(email) + "&password=" + encodeURIComponent(password)
+        body: "option=login" +
+              "&email=" + encodeURIComponent(email) +
+              "&password=" + encodeURIComponent(password)
     })
     .then(response => response.json())
     .then(data => {
-        if (data.ok) {
+        if (data.response == "00") {
             alert("Bienvenida " + data.nombre);
             window.location.href = "principal.html";
         } else {
-            alert(data.mensaje);
+            alert(data.message);
         }
     })
     .catch(error => console.log(error));

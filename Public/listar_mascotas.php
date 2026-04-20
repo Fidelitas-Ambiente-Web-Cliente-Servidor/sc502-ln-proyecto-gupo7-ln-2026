d047e7d5c38d380e92341a0910f8733103dd8482
Natalia Campos A <nataliacamposagui1302@gmail.com>
2026-04-20 12:26:30 -0600

    Buscador funcional
    Funciona

--- Public/listar_mascotas.php
+++ Public/listar_mascotas.php
@@ -4,9 +4,27 @@ require_once __DIR__ . "/../config/Database.php";
 $db = new Database();
 $conn = $db->connect();
 
-$sql = "SELECT * FROM mascotas";
-$resultado = $conn->query($sql);
+// 🔍 recibir búsqueda por nombre
+$nombre = $_GET["nombre"] ?? "";
 
+// 🔥 si escribió algo, filtra
+if ($nombre != "") {
+
+    $sql = "SELECT * FROM mascotas WHERE nombre LIKE ?";
+    $stmt = $conn->prepare($sql);
+
+    $busqueda = "%" . $nombre . "%";
+    $stmt->bind_param("s", $busqueda);
+    $stmt->execute();
+    $resultado = $stmt->get_result();
+
+} else {
+    // 🔄 si no busca nada, trae todo
+    $sql = "SELECT * FROM mascotas";
+    $resultado = $conn->query($sql);
+}
+
+// 🔥 mostrar tarjetas
 while($fila = $resultado->fetch_assoc()) {
 
     echo '
